bb1d9a0427b699f9c4c5e8bb48e006f9367d4f12
lucas <lucaseschultz@gmail.com>
2024-09-28 09:58:18 -0600

    Completed next.js learning chapter 9

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,11 @@
+// pnpm dev
+// http://localhost:3000/
+// http://localhost:3000/dashboard
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+// import {SERVER_PROPS_EXPORT_ERROR} from "next/dist/lib/constants";
 
 export default function RootLayout({
   children,
@@ -8,7 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+        <head>
+            <title>Next.js</title>
+        </head>
+        <body className={`${inter.className} antialiased`}>
+            {children}
+            <SpeedInsights />
+        </body>
     </html>
   );
 }
