6d3bb0421dce1975cecc303c85b292bb999fa3b7
lucas <lucaseschultz@gmail.com>
2025-01-23 13:34:39 -0700

    Completed next.js learning

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -2,11 +2,20 @@
 // Email: user@nextmail.com
 // Password: 123456
 
-
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 // import {SERVER_PROPS_EXPORT_ERROR} from "next/dist/lib/constants";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
